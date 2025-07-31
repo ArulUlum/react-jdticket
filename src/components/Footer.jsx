3b87d2c350275771b39f9f75b1fd03db27102f89
Andi Muh. Syahrul Ulum <syahrululum1911@gmail.com>
2025-07-31 18:16:11 +0700

    edit email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,7 +22,7 @@ function Footer() {
       </div>
       {/* Right: Social Icons */}
       <div className="flex items-center gap-6">
-        <a href="kebbu.workspace@gmail.com" target="_blank" rel="noopener noreferrer" className="text-[#a2a2a2] hover:text-white transition"><Mail size={22} /></a>
+        <a href="mailto:kebbu.workspace@gmail.com" target="_blank" rel="noopener noreferrer" className="text-[#a2a2a2] hover:text-white transition"><Mail size={22} /></a>
         <a href="https://instagram.com/kebbu.id" target="_blank" rel="noopener noreferrer" className="text-[#a2a2a2] hover:text-white transition"><Instagram size={22} /></a>
         <a href="https://x.com/kebbu_id" target="_blank" rel="noopener noreferrer" className="text-[#a2a2a2] hover:text-white transition"><X size={22} /></a>
       </div>
